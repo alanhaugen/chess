16098f35fa1cb7979eb2648f196726753cd2e861
Alan <alanhaugen@gmail.com>
2023-04-06 18:41:09 +0200

    Set position for test text to 100,100

--- source/chess.cpp
+++ source/chess.cpp
@@ -34,7 +34,7 @@ void Chess::Init()
     light     = new PointLight(glm::vec3(-9.683014, 16.498363, 7.318779));
     board     = new Board();
     pointer   = new Sprite("data/cursor.png", 0, 0, 50, 30);
-    text      = new Text("Test string");
+    text      = new Text("Test string", 100, 100);
 
     fpsCamera->UpdateCamera();
 
